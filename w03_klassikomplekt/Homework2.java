087cd1b3791a57bf99724dacb98ed9e6daa70cb1
Elena <55079917+elenadun@users.noreply.github.com>
2021-02-26 14:08:02 +0200

    Kolmanda tunni kodutöö

---
+++ w03_klassikomplekt/Homework2.java
@@ -0,0 +1,26 @@
+class Homework2 {
+    public static void main(String[] args) {
+        // 1. ülesanne
+        String str = "Programmeerimine";  
+
+        System.out.println(str.length()); // prindib välja kui palju tähti esineb sõnas
+
+        //2. ülesanne
+        String mrk = "?!.:<>";
+
+        System.out.println(mrk.length()); // prindib välja kui palju tähemärki esineb 
+
+        // 3.ülesanne
+        String name = "Elena";
+
+        System.out.println(name.toUpperCase()); // muudab tähed suureks
+
+        // 5.ülesanne 
+        int a = -100;
+        int b = -55;
+
+        System.out.println(Math.max(a, b)); // prindib välja max arv
+
+      }
+
+}
