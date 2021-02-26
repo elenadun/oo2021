6eacf60c615d38c0257ee03256c954c27633f882
Elena <55079917+elenadun@users.noreply.github.com>
2021-02-26 14:05:45 +0200

    Kolmanda tunni kodutöö

---
+++ w03_klassikomplekt/Shop.java
@@ -0,0 +1,14 @@
+public class Shop{ // klass pood
+    int shopAmount;
+    int newShopAmount;
+
+    //konstruktor
+    public Shop(int nShopAmount){
+        shopAmount = nShopAmount;
+    }
+
+    public int askShopAmount(){
+        return shopAmount;
+    }
+
+}
